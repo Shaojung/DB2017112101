3bdbc68e75b7cafb48a0f20353e6d886adfd4874
Shaojung <shaojung@gmail.com>
2017-11-21 20:17:55 +0800

    base okay.

--- app/src/main/java/com/example/student/db2017112101/MyAdapter.java
+++ app/src/main/java/com/example/student/db2017112101/MyAdapter.java
@@ -1,10 +1,13 @@
 package com.example.student.db2017112101;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,11 +18,13 @@ import java.util.ArrayList;
 
 public class MyAdapter extends BaseAdapter {
     ArrayList<String> data;
+    boolean chkArray[];
     Context context;
     public MyAdapter(Context context, ArrayList<String> data)
     {
         this.context = context;
         this.data = data;
+        chkArray = new boolean[data.size()];
     }
 
     @Override
@@ -43,6 +48,16 @@ public class MyAdapter extends BaseAdapter {
         View v = inflater.inflate(R.layout.myitem, null);
         TextView tv = (TextView) v.findViewById(R.id.textView);
         tv.setText(data.get(i));
+        final int p = i;
+        CheckBox chk = v.findViewById(R.id.checkBox);
+        chk.setChecked(chkArray[i]);
+        chk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                chkArray[p] = b;
+            }
+        });
+        Log.d("getView", "position:" + i);
         return v;
     }
 }
