8d00d5d00200b22017ec0cac974deb6587bb802f
Shaojung <shaojung@gmail.com>
2017-11-21 20:54:15 +0800

    base okay.

--- app/src/main/java/com/example/student/db2017112101/MainActivity.java
+++ app/src/main/java/com/example/student/db2017112101/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.student.db2017112101;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.DataOutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +27,29 @@ public class MainActivity extends AppCompatActivity {
         lv = (ListView) findViewById(R.id.listView);
         adapter = new MyAdapter(this, mylist);
         lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("SHOW");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle().equals("SHOW"))
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i=0;i<mylist.size();i++)
+            {
+                if (adapter.chkArray[i])
+                {
+                    sb.append(mylist.get(i));
+                    sb.append(",");
+                }
+            }
+            Toast.makeText(MainActivity.this, sb.toString(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
